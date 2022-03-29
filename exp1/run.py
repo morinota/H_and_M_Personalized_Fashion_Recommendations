0e0cf1f429290bc2714256c778ec870b937671f9
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-03-29 17:59:56 +0900

    ss

--- exp1/run.py
+++ exp1/run.py
@@ -1,9 +1,9 @@
 from data import load_data
-from train import read_data
+from preprocessing import read_data
 def main():
     load_data()
     df, df_sub, dfu, dfi = read_data()
-    
+
 
 
 if __name__ == '__main__':
