7fe510baa0a561a81dc80e6cef9b4a7c8a35ea27
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-04-11 04:25:46 +0900

    a

--- approaches/often_by_that_too.py
+++ approaches/often_by_that_too.py
@@ -1,1 +1,176 @@
-#「この商品を買った客は、あの商品もよく買ってるぞ！」という考えをベースにしたレコメンド手法
+# 「この商品を買った客は、あの商品もよく買ってるぞ！」という考えをベースにしたレコメンド手法
+import imp
+import os
+import pandas as pd
+import implicit
+import numpy as np
+import scipy.sparse
+import json
+from dataset import DataSet
+from typing import List, Dict
+from tqdm import tqdm
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+
+
+class OftenBuyThatToo:
+    # クラス変数の定義
+    INPUT_DIR = r"input"
+    DRIVE_DIR = r'/content/drive/MyDrive/Colab Notebooks/kaggle/H_and_M_Personalized_Fashion_Recommendations'
+
+    def __init__(self, transaction_train: pd.DataFrame) -> None:
+        # インスタンス変数(属性の初期化)
+        self.ALL_ITEMS = []
+        self.ALL_USERS = []
+        self.transaction_train = transaction_train
+        pass
+
+    def create_ranking(self, dataset: DataSet):
+
+        # 年齢層ごとに別のランキングを作りたいので、客がどの年齢層に所属しているかを示すカラムを作っていく。
+        listBin = [-1, 19, 29, 39, 49, 59, 69, 119]
+        dataset.dfu['age_bins'] = pd.cut(
+            dataset.dfu['age'], listBin).astype(str)
+
+        # 年齢層グルーピングのSeriesを取得
+        cus_agebins = dataset.dfu['age_bins'].astype(str)
+        # 年齢層グルーピングのユニーク値のリストを取得
+        listUniBins = dataset.dfu['age_bins'].unique().tolist()
+
+        # まずは、「各ユーザが買った商品一覧」のDictを作っていく。
+        # 結果格納用のdictをInitialize
+        ds_dict_c_a: Dict[str, List[str]] = {}
+
+        # 学習期間の、トランザクション1つ1つに対して処理を実行
+        df = self.transaction_train
+        for i in tqdm(range(len(df))):
+            # トランザクションのユーザid、アイテムidを取得.
+            customer_id = df.loc[i, "customer_id"]
+            article_id = df.loc[i, "article_id"]
+
+            # もし既にユーザidがdictのkeyに登録されていれば。。。
+            if customer_id in ds_dict_c_a:
+                # アイテムidをvalueに追加(=valueはListなので、下記のように連結で書ける)
+                ds_dict_c_a[customer_id] = ds_dict_c_a[customer_id] + \
+                    [article_id]
+            else:
+                # 未登録のユーザであれば、key:valueとして新規登録.
+                ds_dict_c_a[customer_id] = [article_id]
+
+        # 作成したdictをインスタンス変数に保存
+        self.c_a_dict = ds_dict_c_a
+
+        # 次に、「ある商品を買った客一覧」のDictを作っていく。
+        # 結果格納用のdictをInitialize。各年齢層グルーピング毎に、「ある商品を買った客一覧」のDictを格納する。
+        ds_dict_a_c: Dict[str, Dict[int, List[str]]] = {
+            listUniBins[i]: {} for i in range(len(listUniBins))}
+
+        # 学習期間の、トランザクション1つ1つに対して処理を実行
+        for i in tqdm(range(len(df))):
+            # トランザクションのユーザid、アイテムidを取得.
+            customer_id = df.loc[i, "customer_id"]
+            article_id = df.loc[i, "article_id"]
+            # ユーザのage_binを取得.
+            age_bin = cus_agebins[customer_id]
+            # 各年齢層毎の「ある商品を買った客一覧」のDictに、対象アイテムをkeyで登録していく。
+            # valueは空のリスト。考えてみると、この方法が可読性高いかも。
+            ds_dict_a_c[age_bin][int(article_id)] = []
+
+        # 再度、学習期間の、トランザクション1つ1つに対して処理を実行
+        # Initializeした、対象アイテム：空のリストに、要素(customer_id)を追加していく。
+        for i in tqdm(range(len(df))):
+            customer_id = df.loc[i, "customer_id"]
+            article_id = df.loc[i, "article_id"]
+            age_bin = cus_agebins[customer_id]
+            ds_dict_a_c[age_bin][int(article_id)] += [customer_id]
+
+        # 作成したdictをインスタンス変数に保存
+        self.a_c_dict = ds_dict_c_a
+
+        # 買った商品集計リストを作成しやすくするために、article_idを空間圧縮
+        df_a_i = {}
+        df_i_a = {}
+        # 全アイテムに対して、繰り返し処理
+        for i in range(len(dataset.dfi)):
+            # article_idと、空間圧縮したindexの対応表dictを生成。
+            df_a_i[dataset.dfi.loc[i, "article_id"]] = i
+            df_i_a[i] = dataset.dfi.loc[i, "article_id"]
+
+        # いよいよ、「ある商品を買った人が他に買っている商品ランキング」を作成していく。
+        df_articles = dataset.dfi.copy()
+        # 結果格納用のDataFrameをInitialize
+        cols = ["article_id", "pred_id", "confidence"]
+        table = pd.DataFrame(index=[], columns=cols)
+
+        # 各年齢グルーピング毎に、繰り返し処理していく。
+        for uniBin in listUniBins:
+            # グルーピングがnanなら次のループへ
+            if uniBin == 'nan':
+                continue
+
+            # 結果格納用のTemporalな変数をInitialize
+            article_id = []
+            pred_id = []
+            confidence = []
+
+            # 各「あるアイテム」と「ある商品を買った客一覧」毎に、繰り返し処理していく
+            for articl, coslist in tqdm(self.a_c_dict[uniBin].items()):
+
+                # 「ある商品を買った人が他に買っている商品」をカウントするListをInitialize
+                count = [0]*len(df_articles)
+
+                # 「ある商品を買った客」毎に繰り返し処理
+                for costomer in coslist:
+                    # 各ユーザが購入したアイテム毎に繰り返し処理
+                    for x in ds_dict_c_a[costomer]:
+                        # 「ある商品を買った人が他に買っている商品」Listにカウント
+                        count[df_a_i[x]] += 1
+
+                # 「あるアイテム」のidを100個格納したリスト(?)を生成
+                art_list = [articl]*100
+                # 「あるアイテム」に対して、「ある商品を買った人が他に買っている商品」カウントを降順に並べ替えたリストを生成。
+                pred_list = sorted(range(len(df_articles)),
+                                   key=lambda k: count[k], reverse=True)[:100]
+
+                # 一緒に買われるアイテム上位100個に対して繰り返し処理：
+                for i in range(len(pred_list)):
+                    # 結果格納用のListに格納していく。
+                    pred_list[i] = df_i_a[pred_list[i]]
+
+                # カウント自体も信頼度を示す指標としてListに保存
+                conf_list = sorted(count, reverse=True)[:100]
+
+                # 結果格納用(全アイテム)のリストに、各アイテムの結果を追加。
+                article_id.extend(art_list)
+                pred_id.extend(pred_list)
+                confidence.extend(conf_list)
+                del art_list
+                del pred_list
+                del conf_list
+
+            table = pd.DataFrame(
+                list(zip(article_id, pred_id, confidence)), columns=cols)
+            # 結果を保存.
+            table.to_pickle(os.path.join(OftenBuyThatToo.DRIVE_DIR,
+                            f"items_of_other_costomers_{uniBin}.pkl"))
+
+
+        with open("items_of_other_costomers.json", mode="w") as f:
+            ranking = json.dumps(ranking, cls = MyEncoder)
+            f.write(ranking)        
+
+    def load_ranking(self):
+        # 本番レコメンド用のjsonデータをロードする関数
+
+        # 「ある商品を買った人が他に買った商品ランキング」
+        with open(os.path.join(OftenBuyThatToo.DRIVE_DIR, "items_of_other_costomers.json"), mode="r") as f:
+            self.ds_dict = json.load(f)
+        # 「ある客が買った商品一覧」の辞書
+        with open(os.path.join(OftenBuyThatToo.DRIVE_DIR, "dict_c_a.json"), mode="r") as f:
+            self.c_a_dict = json.load(f)
+
+    def create_recommendation(self):
+        pass
---
+++ run_often_by_that_too_valid.py
@@ -0,0 +1,61 @@
+from tokenize import group
+from approaches.often_by_that_too import OftenBuyThatToo
+from kaggle_api import load_data
+from dataset import DataSet
+from approaches.last_purchased import last_purchased_items
+from partitioned_validation import partitioned_validation, user_grouping_online_and_offline
+from recommend_results import RecommendResults
+from oneweek_holdout_validation import get_train_oneweek_holdout_validation, get_valid_oneweek_holdout_validation
+
+DRIVE_DIR = r'/content/drive/MyDrive/Colab Notebooks/kaggle/H_and_M_Personalized_Fashion_Recommendations'
+
+
+def main():
+    # kaggle APIからデータロード
+    load_data()
+    # DataSetオブジェクトの読み込み
+    dataset = DataSet()
+    # DataFrameとしてデータ読み込み
+    dataset.read_data()
+    print("1")
+
+    # One-week hold-out validation
+    val_week_id = 104
+    val_df = get_valid_oneweek_holdout_validation(
+        dataset=dataset,  # type: ignore
+        val_week_id=val_week_id
+    )
+    train_df = get_train_oneweek_holdout_validation(
+        dataset=dataset,
+        val_week_id=104,
+        training_days=9999,
+        how="from_init_date_to_last_date"
+    )
+    print(len(train_df))
+
+    print("2")
+
+    # 全ユーザをグルーピング
+    group_series = user_grouping_online_and_offline(dataset=dataset)
+    print(type(group_series))
+    print("2")
+
+    # レコメンド結果を作成し、RecommendResults結果に保存していく。
+    recommend_results_valid = RecommendResults()
+    # とりあえずLast Purchased Item
+    model = OftenBuyThatToo(transaction_train=train_df)
+    model.create_ranking(dataset=dataset)
+
+    print("3")
+
+    # # One-week hold-out validationのオフライン評価
+    # score = partitioned_validation(val_df=val_df,
+    #                                pred_df=df_pred,
+    #                                grouping=group_series,
+    #                                approach_name="last_purchased_items"
+    #                                )
+    # print(score.head())
+
+
+if __name__ == '__main__':
+    main()
