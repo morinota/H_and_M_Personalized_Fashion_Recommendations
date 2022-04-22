b3491b8a3d599a9a727666bcb6490cf1c31c03cf
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-04-22 11:54:16 +0900

    a

--- models/contentBase_image.py
+++ models/contentBase_image.py
@@ -1,8 +1,94 @@
-
-from typing import List, Tuple
+import pickle
+from typing import Dict, List, Tuple
 import pandas as pd
 from my_class.dataset import DataSet
 from utils.useful_func import iter_to_str
-from collections import defaultdict
+import numpy as np
+from tqdm import tqdm
+import os
+import random
 
-        
+INPUT_DIR = r"input"
+DRIVE_DIR = r'/content/drive/MyDrive/Colab Notebooks/kaggle/H_and_M_Personalized_Fashion_Recommendations'
+
+
+class ContentBaseImage:
+
+    def __init__(self, transaction_train: pd.DataFrame, dataset: DataSet) -> None:
+        # インスタンス変数(属性の初期化)
+        self.dataset = dataset
+        self.transaction_train = transaction_train
+        self.ALL_ITEMS = []
+        self.ALL_USERS = []
+        self.hyper_params = {}
+
+    def _load_similarity_data(self):
+        self.kNN_json_path = os.path.join(
+            DRIVE_DIR, 'input/nearest_neighbor_dictionary.json')
+        # 類似度のkNNデータを読み込み
+        a_file = open(self.kNN_json_path, "rb")
+        self.nearest_neighbor_dictionary: Dict[str, Dict[str, List]]
+        self.nearest_neighbor_dictionary = pickle.load(a_file)
+
+    def _create_each_customer_transaction(self):
+        self.customer_transactions = self.transaction_train.groupby(
+            by="customer_id_short")['article_id'].agg(list).reset_index()
+
+    def _create_dummy_recommendation(self):
+        self.most_bought_articles = list(
+            (self.transaction_train['article_id'].value_counts()).index)[:12]
+        self.most_bought_articles = ' '.join(
+            [str(item).zfill(10) for item in self.most_bought_articles])
+
+    def preprocessing(self):
+        self._load_similarity_data()
+        self._create_each_customer_transaction()
+        self._create_dummy_recommendation()
+
+    def _arrange_for_submission(self):
+        # submission用に加工
+        df_sub = pd.merge(self.dataset.df_sub[['customer_id', 'customer_id_short']], self.customer_transactions, on='customer_id_short', how='left')
+        df_sub.rename(columns={"nearest_article_ids":'prediction'}, inplace=True)
+        self.df_sub = df_sub[['customer_id', 'customer_id_short', 'prediction']]
+
+    def create_recommendation(self):
+
+        def _add_neighbors(article_ids):
+            """
+            各ユーザの学習期間における、購入アイテムリストを受け取り、
+            kNNをベースにコンテンツベースのレコメンドアイテムのListを返す関数。
+            """
+            nearest_articles = []
+            for article_id in article_ids:
+
+                article_id = str(article_id).zfill(10)
+                try:
+                    # 各購入アイテム毎に近傍アイテムを取得し、リストに追加
+                    nearest_articles = nearest_articles + \
+                        self.nearest_neighbor_dictionary[article_id]['nn_article_id'][1:]
+
+                except:
+                    continue
+
+            # 2重のリストを一重に変換
+            # nearest_articles = nearest_articles
+
+            # 12個にする処理.
+            if len(nearest_articles) > 12:
+                # ランダムに12個を選ぶ.
+                nearest_articles = random.sample(nearest_articles, 12)
+            elif len(nearest_articles) < 12:
+                nearest_articles.extend(random.sample(
+                    self.most_bought_articles, 12 - len(nearest_articles)))
+            return nearest_articles
+
+        # 学習期間内にアイテムを購入した各ユーザに対して、レコメンド
+        self.customer_transactions["nearest_article_ids"] = self.customer_transactions.apply(
+            lambda row: _add_neighbors(row["article_id"]), axis=1)
+        # レコメンド商品のリストを文字列へ
+        self.customer_transactions["nearest_article_ids"] = self.customer_transactions["nearest_article_ids"].apply(
+            lambda x: " ".join(x))
+
+        self._arrange_for_submission()
+
+        return self.df_sub
---
+++ run_contentBase_image.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+from models.contentBase_image import ContentBaseImage
+from tqdm import tqdm
+from datetime import datetime
+from my_class.dataset import DataSet
+from utils.partitioned_validation import partitioned_validation, user_grouping_online_and_offline
+from utils.oneweek_holdout_validation import get_train_oneweek_holdout_validation, get_valid_oneweek_holdout_validation
+from utils.recommend_emsemble import recommend_emsemble
+import os
+from logs.base_log import create_logger, get_logger, stop_watch
+# from logs.time_keeper import stop_watch
+
+DRIVE_DIR = r'/content/drive/MyDrive/Colab Notebooks/kaggle/H_and_M_Personalized_Fashion_Recommendations'
+
+VERSION = "contentBase_image_latestMonth"
+
+
+@stop_watch(VERSION)
+def main(val_week_id=104):
+
+    # DataSetオブジェクトの読み込み
+    dataset = DataSet()
+    # DataFrameとしてデータ読み込み
+    dataset.read_data()
+    print("1")
+
+    # One-week hold-out validation
+    val_df = get_valid_oneweek_holdout_validation(
+        dataset=dataset,  # type: ignore
+        val_week_id=val_week_id
+    )
+    train_df = get_train_oneweek_holdout_validation(
+        dataset=dataset,
+        val_week_id=104,
+        training_days=31,
+        # how="use_same_season_in_past"
+    )
+
+    # 全ユーザをグルーピング
+    grouping_df = user_grouping_online_and_offline(dataset=dataset)
+
+    # レコメンド結果を生成
+    model = ContentBaseImage(transaction_train=train_df, dataset=dataset)
+    model.preprocessing()
+    df_sub = model.create_recommendation()
+
+    # One-week hold-out validationのオフライン評価
+    score_df = partitioned_validation(val_df=val_df,
+                                      pred_df=df_sub,
+                                      grouping=grouping_df['group'],
+                                      approach_name=VERSION
+                                      )
+    # スコアをロギング
+    get_logger(VERSION).info('\t' + score_df.to_string().replace('\n', '\n\t'))
+
+    # レコメンド結果を保存
+    if val_week_id == 104:
+        val_result_dir = os.path.join(DRIVE_DIR, 'val_results_csv')
+        df_sub.to_csv(os.path.join(val_result_dir, f'val_{VERSION}.csv'))
+    
+    if val_week_id == 105:
+        sub_result_dir = os.path.join(DRIVE_DIR, 'submission_csv')
+        df_sub.to_csv(os.path.join(sub_result_dir, f'sub_{VERSION}.csv'))
+
+if __name__ == '__main__':
+    create_logger(VERSION)
+    get_logger(VERSION).info("メッセージ")
+    main(val_week_id=104)
