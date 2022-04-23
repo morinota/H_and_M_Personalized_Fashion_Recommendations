628b35903ab89718bc597f245f3d971ef465fce5
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-04-23 16:37:19 +0900

    a

--- run_lp.py
+++ run_lp.py
@@ -22,7 +22,9 @@ def main(val_week_id=104):
     # DataSetオブジェクトの読み込み
     dataset = DataSet()
     # DataFrameとしてデータ読み込み
-    dataset.read_data()
+    # dataset.read_data()
+    dataset.read_data_sampled()
+
     print("1")
 
     # One-week hold-out validation
@@ -32,54 +34,45 @@ def main(val_week_id=104):
     )
     train_df = get_train_oneweek_holdout_validation(
         dataset=dataset,
-        val_week_id=104,
-        training_days=9999,
-        how="from_init_date_to_last_date"
+        week_column_exist=False,
+        val_week_id=val_week_id,
+        training_days=31,
+        # how="use_same_season_in_past"
     )
 
     # 全ユーザをグルーピング
     grouping_df = user_grouping_online_and_offline(dataset=dataset)
 
     # Last Purchased Itemによるレコメンド結果を生成
-    df_pred = last_purchased.last_purchased_items(train_transaction=train_df,
+    df_sub = last_purchased.last_purchased_items(train_transaction=train_df,
                                                   dataset=dataset)
     
 
     # One-week hold-out validationのオフライン評価
     score_df = partitioned_validation(val_df=val_df,
-                                      pred_df=df_pred,
+                                      pred_df=df_sub,
                                       grouping=grouping_df['group'],
-                                      approach_name="last_purchased_items"
+                                      approach_name=VERSION
                                       )
     # スコアをロギング
+    get_logger(VERSION).info(f'va_week_id is {val_week_id}')
     get_logger(VERSION).info('\t' + score_df.to_string().replace('\n', '\n\t'))
 
-'''
-    predicted_kwargs = {"last_purchased_items": df_pred_1,
-                        "other_colors_of_purchased_item": df_pred_2,
-                        "popular_items_for_each_groupm": df_pred_3
-                        }
-
-    # 3種のレコメンドを結合
-    predicted_weights = [100, 10, 1]
-    df_sub = recommend_emsemble(predicted_kwargs=predicted_kwargs, weight_args=predicted_weights,
-                                dataset=dataset, val_week_id=val_week_id)
-
-    score_df = partitioned_validation(val_df=val_df,
-                                      pred_df=df_sub,
-                                      score=score_df,
-                                      grouping=grouping_df['group'],
-                                      approach_name="blend"
-                                      )
     # レコメンド結果を保存
-    if val_week_id == 104:
-        val_result_dir = os.path.join(DRIVE_DIR, 'val_results_csv')
-        df_sub.to_csv(os.path.join(val_result_dir, 'val_last_purchased.csv'))
-        '''
-if val_week_id == 105:
-    sub_result_dir = os.path.join(DRIVE_DIR, 'submission_csv')
-    df_pred.to_csv(os.path.join(sub_result_dir, 'val_last_purchased.csv'))
+
+    if val_week_id == 105:
+        sub_result_dir = os.path.join(DRIVE_DIR, 'submission_csv')
+        df_sub.to_csv(os.path.join(sub_result_dir, f'sub_{VERSION}.csv'))
+
+    else:
+        val_result_dir = os.path.join(
+            DRIVE_DIR, f'val_results_{val_week_id}_csv')
+        df_sub.to_csv(os.path.join(val_result_dir, f'val_{VERSION}.csv'))
+
+
 if __name__ == '__main__':
     create_logger(VERSION)
     get_logger(VERSION).info("メッセージ")
-    main(val_week_id=104)
+    val_week_ids = [104, 103, 102]
+    for val_week_id in val_week_ids:
+        run_validation(val_week_id=val_week_id)
