33ebc97b0478176f0985ea0e489a3ac8c73b4b25
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-04-28 18:43:21 +0900

    a

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ class Config:
     run_for_submittion = False # bool
 
     # 5%サンプリングを使うか、フルサンプリングを使うか。
-    use_full_sampling = False # bool
+    use_full_sampling = True # bool
 
     # ランク学習用のCandidate
     num_candidate_train = 15 # 訓練用のCandidateの数
-    num_candidate_predict = 15 # 予測用のCandidateの数
+    num_candidate_predict = 12 # 予測用のCandidateの数
     # 予測用のCandidateを、オリジナルの手法を使うか、もしくはどの手法から読み込むか。
     predict_candidate_original = True
     predict_candidate_way_name = 'last_purchased_fullTrue_15Candidates' # Noneだったらオリジナル?
