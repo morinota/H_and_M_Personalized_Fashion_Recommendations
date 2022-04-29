cec454fc107b1f30af1774118eadf87774017bd4
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-04-29 14:05:05 +0900

    change use_full_sampling false

--- models/rank_learning_lgbm.py
+++ models/rank_learning_lgbm.py
@@ -78,8 +78,8 @@ class RankLearningLgbm:
             .apply(lambda x: pd.factorize(x)[0])).astype("uint64")
 
     def _merge_user_item_feature_to_transactions(self):
-        self.df = self.df.merge(self.user_features, on=('customer_id_short'))
-        self.df = self.df.merge(self.item_features, on=('article_id'))
+        self.df = self.df.merge(self.user_features, on=('customer_id_short'), how='left')
+        self.df = self.df.merge(self.item_features, on=('article_id'), how='left')
         # 降順(新しい順)で並び変え
         self.df.sort_values(['t_dat', 'customer_id_short'],
                             inplace=True, ascending=False)
