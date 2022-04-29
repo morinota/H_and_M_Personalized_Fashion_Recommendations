a64801ae31af9919cc1a61d5c5b691949f39f64e
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-04-29 13:00:15 +0900

    change use_full_sampling false

--- models/rank_learning_lgbm.py
+++ models/rank_learning_lgbm.py
@@ -19,8 +19,7 @@ class RankLearningLgbm:
     def __init__(self, transaction_train: pd.DataFrame, dataset: DataSet, val_week_id: int) -> None:
         # インスタンス変数(属性の初期化)
         self.dataset = dataset
-        # self.df = transaction_train
-        self.df = dataset.df
+        self.df = transaction_train
         self.ALL_ITEMS = []
         self.ALL_USERS = []
         self.hyper_params = {}
