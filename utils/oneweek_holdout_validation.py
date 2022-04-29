8d97ba95807e0482e7ca48245e053e32be43b60d
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-04-29 12:56:43 +0900

    change use_full_sampling false

--- utils/oneweek_holdout_validation.py
+++ utils/oneweek_holdout_validation.py
@@ -32,8 +32,14 @@ def get_valid_oneweek_holdout_validation(dataset: DataSet, val_week_id: int = 10
     """
 
     # 元々のtransaction_dfから、検証用weekのtransactionデータのみを抽出
-    val_mask = (dataset.df['week'] == val_week_id)
-    transaction_df_val = dataset.df[val_mask]
+    if 'week' in dataset.df.columns:
+        val_mask = (dataset.df['week'] == val_week_id)
+        transaction_df_val = dataset.df[val_mask]
+    else:
+        last_date_val = pd.to_datetime('2020-09-22') - datetime.timedelta(days=(105-val_week_id)*7)
+        init_date_val = pd.to_datetime(last_date_val) - datetime.timedelta(days=6)
+        val_mask = (dataset.df['t_dat'] <= last_date_val) & (dataset.df['t_dat'] >= init_date_val)
+        transaction_df_val = dataset.df[val_mask]
 
     # 最初の日付と最後の日付を確認
     start_day_val = transaction_df_val['t_dat'].min()
