59fd8e7fc275ed3200793005a3c03019bc385dcd
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-05-01 20:25:41 +0900

    a

--- run_feature_engineering.py
+++ run_feature_engineering.py
@@ -6,7 +6,6 @@ from my_class.dataset import DataSet
 from models.last_purchased_class import LastPurchasedItrems
 from utils.partitioned_validation import partitioned_validation, user_grouping_online_and_offline
 from utils.oneweek_holdout_validation import get_train_oneweek_holdout_validation, get_valid_oneweek_holdout_validation
-from utils.recommend_emsemble import recommend_emsemble
 from utils.just_offline_validation import offline_validation
 import os
 from logs.base_log import create_logger, get_logger, stop_watch
@@ -16,12 +15,11 @@ from feature import create_user_features
 
 DRIVE_DIR = r'/content/drive/MyDrive/Colab Notebooks/kaggle/H_and_M_Personalized_Fashion_Recommendations'
 
-def main():
 
+def main():
 
     create_user_features.create_user_features()
 
 
 if __name__ == '__main__':
     main()
-
