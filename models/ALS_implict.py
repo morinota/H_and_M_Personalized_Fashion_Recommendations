622172d5cf62f2188c515f942896ff8c83b51f05
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-05-08 14:33:19 +0900

    a

--- models/ALS_implict.py
+++ models/ALS_implict.py
@@ -115,8 +115,10 @@ class MatrixFactrization:
         print(type(self.user_matrix))
      
         # 長さを確認
-        print(self.user_matrix_df.shape)
+        print(self.item_matrix.shape)
         print(len(self.ALL_USERS))
+        print(self.item_matrix.shape)
+        print(len(self.ALL_ITEMS))
 
         # DataFrameに加工する
         self.user_matrix_df = pd.DataFrame(
